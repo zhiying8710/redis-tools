08c0f2fd44917dbe2dfa3faa538e1a4cec16d2eb
zhiying8710 <zhiying8710@hotmail.com>
2015-11-24 14:46:20 +0800

    fix test

--- src/me/binge/redis/test/ExecutorTest.java
+++ src/me/binge/redis/test/ExecutorTest.java
@@ -2,6 +2,7 @@ package me.binge.redis.test;
 
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.Set;
 
 import me.binge.redis.exec.RedisExecutor;
 import me.binge.redis.exec.RedisExecutors;
@@ -18,6 +19,25 @@ public class ExecutorTest {
         RedisExecutor<?> executor = RedisExecutors.get(props);
         executor.set("__hahah", "gogogogo");
         System.out.println(executor.get("__hahah"));
+
+        Set<String> nums = executor.zrangeByScore("__order_3D8398C8EDBA6D55609B388B69C52A5B_266589", Double.MIN_VALUE, Double.MAX_VALUE);
+        boolean isOrder = true;
+        int last = -1;
+        for (String num : nums) {
+            int curr = Integer.valueOf(num);
+            if (last == -1) {
+                last = curr;
+                continue;
+            }
+            if (curr < last) {
+                System.out.println(curr + ":" + last);
+                isOrder = false;
+                break;
+            }
+            last = curr;
+        }
+        System.err.println(isOrder);
+
         executor.close();
 
 
